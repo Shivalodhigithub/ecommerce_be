7f622253fc3b23ef0a59ac5992c8d049157979e2
Shivalodhigithub <shivalodhi565@gmail.com>
2024-02-29 11:52:13 +0530

    second commit

---
+++ model/user.model.js
@@ -0,0 +1,36 @@
+const mongoose=require('mongoose')
+
+//create schema 
+const userSchema=mongoose.Schema({
+    name:{
+        type:String,
+        required:true,  
+    },
+    user_id:{
+        type:String,
+        unique:true,
+        required:true
+    },
+    password:{
+        required:true,
+        type:String
+    },
+    email:{
+        type:String,
+        required:true,
+        minLength:12,
+        unique:true,
+        lowercase:true,
+    },
+    type:{
+        type:String,
+        default:"Customer",
+        enum:["Customer","Admin"]
+    }
+},{timestamps:true,versionkey:false})
+
+//create collection 
+// const user=mongoose.model("user",userSchema);
+// module.exports=user;
+
+module.exports=mongoose.model("user",userSchema);
---
+++ server.js
@@ -0,0 +1,56 @@
+
+/* This file will start server */ 
+
+const express=require('express')
+const app=express(); 
+const mongoose=require('mongoose')
+const bcrypt=require('bcryptjs')
+const server_cofig=require('./config/server.config')
+const db_config=require('./config/db.config')
+const user_model=require('./model/user.model')
+
+/* conncet with mongo db */ 
+mongoose.connect(db_config.DB_URL)
+
+const db=mongoose.connection; 
+
+db.on('error',()=>{
+    console.log("Error while connectiong the data base")
+})
+
+db.once('open',()=>{
+    console.log("Succesfully connected with mongo db!")
+    init();
+})
+
+const init=async()=>{
+    let user=await user_model.findOne({user_id:"admin"}); 
+
+    if(user){
+        console.log('Admin is already exist')
+    }
+    else{
+        try {
+            user=await user_model.create({
+                name:"kallu",
+                user_id:"admin",
+                email:"kallo45@gmail.com",
+                password:bcrypt.hashSync("WELCOME1",8),
+                type:"Admin"
+            })
+            console.log("User is created!",user);
+            
+        } catch (error) {
+            console.log("Error while creating the user",error)
+        }
+    }
+}
+
+/**
+ * Server is running on port
+ * 
+ */
+
+app.listen(server_cofig.PORT,()=>{
+    console.log(`server is running on port ${server_cofig.PORT}`);
+})
