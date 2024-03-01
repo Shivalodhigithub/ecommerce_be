a855eab65d8c857860664bf69900009cc549e2b5
Shivalodhigithub <shivalodhi565@gmail.com>
2024-03-01 22:57:52 +0530

    second commit

---
+++ middle_ware/auth.mw.js
@@ -0,0 +1,44 @@
+const user_model=require('../model/user.model')
+/**
+ * create middle ware to check request body 
+ 
+ */
+
+const verifyReq_body=async(req,res,next)=>{
+   
+    try {
+         // 1.check for user  
+         if(!req.body.name){
+            return res.status(400).send({
+                message:"Error Name was not fill"
+            })
+         }
+
+          // 2.check for email
+          if(!req.body.email){
+            return res.status(400).send({
+                message:"Error email was not fill"
+            })
+         }
+
+         // 3.check for user id  alredy prsent on not 
+         const user = await user_model.findOne({user_id:req.body.user_id})
+         if(user){
+            return res.status(400).send({
+                message:"User Id already exist"
+            })
+         }  
+
+         next(); // it for pass control to next middle ware
+    } catch (error) {
+        console.log("catch error",error)
+        res.status(500).send({
+            message:"Error while checking the request body"
+        })
+        
+    }
+}
+
+module.exports={
+    verifyReq_body:verifyReq_body
+};
--- route/auth.route.js
+++ route/auth.route.js
@@ -3,8 +3,9 @@
  * I need to intercept this
  */
 const authController=require('../controller/auth.controller')
+const authMiddleWare=require('../middle_ware/auth.mw')
 module.exports=(app)=>{
     // app.post('/eccom_db/app/v1/auth/signup',hand over to right controller)
 
-    app.post('/ecomm/api/v1/auth/signup',authController.signup);
+    app.post('/ecomm/api/v1/auth/signup',[authMiddleWare.verifyReq_body],authController.signup);
 }
