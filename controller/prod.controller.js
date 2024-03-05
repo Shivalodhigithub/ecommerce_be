6e82462e4417a3563f83a8f5d480bd0b0d014024
Shivalodhigithub <shivalodhi565@gmail.com>
2024-03-05 23:50:42 +0530

    5th march

--- controller/prod.controller.js
+++ controller/prod.controller.js
@@ -28,3 +28,63 @@ exports.create_product = async (req, res) => {
         })
     }
 }
+
+exports.get_prods=async(req,res)=>{
+    
+    try {
+        const prods=await prod_model.find();
+        res.status(200).send(prods);
+        
+    } catch (error) {
+        console.log("Error while getting products",error); 
+        res.status(500).send({
+            message:"Error while getting products"
+        })
+        
+    }
+}
+
+exports.singl_prod=async(req,res)=>{
+    // const _id=req.params
+    // console.log(_id);
+    // console.log(req.params.id)
+    // res.send(req.params.id)
+    try {
+        const id=await prod_model.findOne({_id:req.params.id})
+        console.log(id);
+        if(!id){
+            return res.status(400).send({
+                message:"Id does not available"
+            })
+        }
+        return res.status(400).send(id);
+        
+    } catch (error) {
+        console.log("Error while fetching single prodt",error)
+        return res.status(500).send({
+            message:"Error while fetching single product:"
+        })
+        
+    }
+}
+
+/** 
+ * update the product 
+ */
+
+exports.update_prod=async(req,res)=>{
+    try {
+        const id=req.params.id; 
+        const updated_prod=await prod_model.findByIdAndUpdate({_id:id},req.body)
+        console.log(updated_prod)
+        return res.status(200).send(updated_prod)
+  
+        
+    } catch (error) {
+        console.log("Error while updating the product",error)
+        res.status(500).send({
+            message:"Error while updating the product"
+        })
+        
+    }
+}
--- route/prod.route.js
+++ route/prod.route.js
@@ -6,4 +6,15 @@ const prod_controller=require('../controller/prod.controller')
 const middle_ware=require('../middle_ware/auth.mw')
 module.exports=(app)=>{
     app.post('/ecom_db/api/create_category',[middle_ware.verify_token,middle_ware.isAdmin],prod_controller.create_product);
+
+
+    // localhost:8888/ecom_db/api/getting_category
+  app.get('/ecom_db/api/getting_category',prod_controller.get_prods);
+
+  //get single user localhost:8888/ecom_db/api/getting_category/65e414f0def5e5fdd6fc394a
+  app.get('/ecom_db/api/getting_category/:id',
+  prod_controller.singl_prod);
+
+//   update the product name localhost:8888/ecom_db/api/getting_category/65e414f0def5e5fdd6fc394a 
+app.put("/ecom_db/api/getting_category/:id",prod_controller.update_prod)
 }
